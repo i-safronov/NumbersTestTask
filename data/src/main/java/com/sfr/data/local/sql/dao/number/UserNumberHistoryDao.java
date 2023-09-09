8f095c57cd4a68f5487aa7143f70e1d70c781aa6
safronov <ilya.safa1337w@gmail.com>
2023-09-09 10:31:34 +0500

    Changed UserNumberHistoryDao

--- data/src/main/java/com/sfr/data/local/sql/dao/number/UserNumberHistoryDao.java
+++ data/src/main/java/com/sfr/data/local/sql/dao/number/UserNumberHistoryDao.java
@@ -2,6 +2,7 @@ package com.sfr.data.local.sql.dao.number;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import com.sfr.data.local.sql.db.model.UserNumberHistoryEntity;
@@ -13,7 +14,7 @@ import kotlinx.coroutines.flow.Flow;
 @Dao
 public interface UserNumberHistoryDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insertNewUserNumber(UserNumberHistoryEntity userNumberHistoryEntity);
 
     @Query("SELECT * FROM USER_NUMBER_HISTORY_TABLE")
