ed04244d74cef959f67a1754ad1b532185660de4
safronov <ilya.safa1337w@gmail.com>
2023-09-11 13:46:38 +0500

    Preparing to add Dagger to the app layer

---
+++ app/src/main/java/com/sfr/numberstesttask/presentation/screen/fragment/main_page/view_model/FragmentMainPageViewModel.java
@@ -0,0 +1,13 @@
+package com.sfr.numberstesttask.presentation.screen.fragment.main_page.view_model;
+
+import androidx.lifecycle.ViewModel;
+
+public class FragmentMainPageViewModel extends ViewModel {
+
+    public FragmentMainPageViewModel() {
+        super();
+    }
+
+
+
+}
