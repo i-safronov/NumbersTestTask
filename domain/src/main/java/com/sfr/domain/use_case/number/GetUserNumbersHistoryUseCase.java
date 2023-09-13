57925c129aa254c8eb4d38d278d9fadc76c95e62
safronov <ilya.safa1337w@gmail.com>
2023-09-13 14:23:29 +0500

    Changed the logic of GetUserNumbersHistoryUseCase

--- domain/src/main/java/com/sfr/domain/use_case/number/GetUserNumbersHistoryUseCase.java
+++ domain/src/main/java/com/sfr/domain/use_case/number/GetUserNumbersHistoryUseCase.java
@@ -3,9 +3,11 @@ package com.sfr.domain.use_case.number;
 import com.sfr.domain.model.UserNumberHistory;
 import com.sfr.domain.repository.NumberRepositoryInt;
 
+import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Function;
 
 public class GetUserNumbersHistoryUseCase {
 
@@ -16,7 +18,13 @@ public class GetUserNumbersHistoryUseCase {
     }
 
     public Observable<List<UserNumberHistory>> execute() {
-        return numberRepositoryInt.getUserNumbersHistory();
+        return numberRepositoryInt.getUserNumbersHistory().map(new Function<List<UserNumberHistory>, List<UserNumberHistory>>() {
+            @Override
+            public List<UserNumberHistory> apply(List<UserNumberHistory> list) throws Throwable {
+                Collections.reverse(list);
+                return list;
+            }
+        });
     }
 
 }
